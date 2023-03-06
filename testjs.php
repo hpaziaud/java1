2244629f0bd61b3769074eceae036d60428589bf
hpaziaud <paziaud.hassan@gmail.com>
2023-03-06 09:59:24 +0100

    rgregt

---
+++ testjs.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Calcul du factoriel</title>
+	<script>
+		function calculerFactoriel() {
+			let n = prompt("Entrez un chiffre N :");
+			let factoriel = 1;
+			for (let i = 1; i <= n; i++) {
+			  factoriel *= i;
+			}
+			alert("Le factoriel de " + n + " est " + factoriel);
+		}
+	</script>
+</head>
+<body>
+	<h1>Calcul du factoriel</h1>
+	<button onclick="calculerFactoriel()">Calculer le factoriel</button>
+</body>
+</html>
