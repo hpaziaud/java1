8e17658b0bc80078441b7f6326ff272faddfcafa
hpaziaud <paziaud.hassan@gmail.com>
2023-03-08 15:02:57 +0100

    gfrrege

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
+function change(){ 
+var madiv1 = document.getElementById("madiv1");
 
-Element madiv1 = document.getElementById("madiv1");
-
-// Changer le contenu de la div
 madiv1.innerHTML = "hello";
+}
+
+change();
